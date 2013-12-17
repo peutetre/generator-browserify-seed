a9efa28a2215a58609e2d2520b4bc9087751e0d8
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-12-17 23:01:35 +0100

    wip

--- app/index.js
+++ app/index.js
@@ -4,17 +4,21 @@ var util = require('util'),
     yeoman = require('yeoman-generator');
 
 var BrowserifySeedGenerator = module.exports = function BrowserifySeedGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({
-        skipInstall: true,
-        skipMessage: true
+    this.on('end', function () {
+        this.installDependencies({
+            skipInstall: true,
+            skipMessage: true
+        });
     });
-  });
 
-  this.userSettings = options.userSettings;
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    // add all user settings from generator-mobile-app
+    for (var setting in options.userSettings)
+        this[setting] = options.userSettings[setting];
+
+    this.deps = [];
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(BrowserifySeedGenerator, yeoman.generators.Base);
@@ -22,37 +26,38 @@ util.inherits(BrowserifySeedGenerator, yeoman.generators.Base);
 function askForDependencies(done) {
     this.prompt([{
         type : 'input',
-        name : 'deps',
+        name : 'dependencies',
         message : 'Add your dependencies separated with comma:'
     }], function (props) {
         this.dependencies = props.dependencies.split(',')
-            .filter(function (dep) { return dep.length > 0; })
-            .map(function (dep) { return dep.trim(); });
-        done();
+        .filter(function (dep) { return dep.length > 0; })
+        .map(function (dep) { return dep.trim(); });
+    done();
     }.bind(this));
 }
 
 BrowserifySeedGenerator.prototype.askUserFor = function askFor() {
-  var cb = this.async();
+    var cb = this.async();
 
-  var prompts = [{
-    type:'confirm',
-    name:'addDeps',
-    message:'Do you want to add some dependencies?',
-    default:true
-  }];
+    var prompts = [{
+        type:'confirm',
+            name:'addDeps',
+            message:'Do you want to add some dependencies?',
+        default:true
+    }];
 
-  this.prompt(prompts, function (props) {
-    if (propos.addDeps) this.deps = askForDependencies.call(this, cb);
-    else cb();
-  }.bind(this));
+    this.prompt(prompts, function (props) {
+        if (props.addDeps) this.deps = askForDependencies.call(this, cb);
+        else cb();
+    }.bind(this));
 
 };
 
 BrowserifySeedGenerator.prototype.app = function app() {
-  this.template('_index.html', 'www/index.html');
-  this.mkdir('www/app');
-  this.template('_main.js', 'www/app/main.js');
-  this.mkdir('www/style');
-  this.copy('main.css', 'www/style/main.css');
+    this.mkdir('www/app');
+    this.mkdir('www/style');
+    this.template('_package.json', 'package.json');
+    this.template('_index.html', 'www/index.html');
+    this.template('_main.js', 'www/app/main.js');
+    this.copy('main.css', 'www/style/main.css');
 };
