2e729d023c134902969ab22550d0c53e018f8426
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-12-30 09:05:10 +0100

    wip

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -1,1 +1,11 @@
 /* main.js - main app entry point of <%= appName %> */
+
+'use strict';
+
+var process = require('process');
+
+function main() {
+    alert('app <%= appName %> is ready!\n process title is: ' + process.title);
+}
+
+window.document.addEventListener(window.Codova ? 'deviceready' : 'DOMContentLoaded', main, false);
